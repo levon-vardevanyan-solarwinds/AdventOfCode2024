185cd69b4f5b0b31981db7bb232d8c5e495b4194
Levon Vardevanyan <levon.vardevanyan@n-able.com>
2024-12-26 12:38:39 +0000

    Day 12

--- day12/solution.rkt
+++ day12/solution.rkt
@@ -35,7 +35,7 @@
                      (list (cons (sub1 i) j)       ; up
                            (cons (add1 i) j)       ; down
                            (cons i (sub1 j))       ; left
-                           (cons i (add1 j)))))))) ; down
+                           (cons i (add1 j)))))))) ; right
 
 (define (flood start)
   (let loop ([current start] [stepped (set)])
@@ -54,8 +54,53 @@
 (define (count-fence current)
   (- 4 (set-count (where-to current))))
 
+(define (solve with)
+  (for/sum ([island (islands)])
+    (* (set-count island)
+       (for/sum ([cell island])
+         (with cell)))))
+
 ; part one
-(for/sum ([island (islands)])
-  (* (set-count island)
-     (for/sum ([cell island])
-       (count-fence cell))))
+(solve count-fence)
+
+; nw  n  ne
+;  w  o   e
+; sw  s  se
+(struct win (nw n ne w o e sw s se) #:transparent)
+
+(define (mask from)
+  (define current (hash-ref matrix from))
+  (match-define (cons i j) from)
+  (define (same? pos)
+    (and (in-bounds? pos)
+         (char=? current (hash-ref matrix pos))))
+  (apply win
+         (map same?
+              (list (cons (sub1 i) (sub1 j))    ; nw
+                    (cons (sub1 i) j)           ; n
+                    (cons (sub1 i) (add1 j))    ; ne
+                    (cons i (sub1 j))           ; w
+                    from                        ; o
+                    (cons i (add1 j))           ; e
+                    (cons (add1 i) (sub1 j))    ; sw
+                    (cons (add1 i) j)           ; s
+                    (cons (add1 i) (add1 j))    ; se
+                    ))))
+
+(define (corners-count from)
+  (let ([m (mask from)])
+    (count identity
+           (list
+            ; outside
+            (nor (win-n m) (win-e m))
+            (nor (win-e m) (win-s m))
+            (nor (win-s m) (win-w m))
+            (nor (win-w m) (win-n m))
+            ; inside
+            (and (win-n m) (win-e m) (not (win-ne m)))
+            (and (win-e m) (win-s m) (not (win-se m)))
+            (and (win-s m) (win-w m) (not (win-sw m)))
+            (and (win-w m) (win-n m) (not (win-nw m)))))))
+  
+; part two
+(solve corners-count)
